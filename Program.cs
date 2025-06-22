256666128cd16796bd125a82730ca6d46bdb2ee9
Sanjay Kumar <sanjaydotnetcore@gmail.com>
2025-06-22 22:28:03 +0530

    testing

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ var builder = WebApplication.CreateBuilder(args);
 var config = builder.Configuration;
 var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";// Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConStr")));
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("LiveConStr")));
 builder.Services.AddCors(Options =>
 {
     //Options.AddPolicy(name: MyAllowSpecificOrigins,
